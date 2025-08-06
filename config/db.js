d5b0dea06ca1bf5457d0636b2868db1e73d9d2fe
Nithish3309m <20bscnithish@gmail.com>
2025-08-06 12:07:57 +0530

    fix db  backend

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('MongoDB connected');
-})
-.catch((err) => {
-    console.log('MongoDB connection error:', err);
-});
+const db = mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.log('MongoDB connection error:', err);
+    });
 
 module.exports = db;
